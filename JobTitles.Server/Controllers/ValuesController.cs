deca475228d1cd4e6449ab6dc0b50755c186e047
Camila <camiluc.peli@gmail.com>
2019-07-01 17:59:56 +0200

    Basic project structure.

--- JobTitles.Server/Controllers/ValuesController.cs
+++ JobTitles.Server/Controllers/ValuesController.cs
@@ -10,35 +10,49 @@ namespace JobTitles.Server.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        [HttpGet("{name}")]
+        public string Get(string name)
+        {
+            return "value";
+        }
+
         [HttpPost]
         public void Post([FromBody] string value)
         {
+            
         }
 
-        // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
+
         }
 
-        // DELETE api/values/5
+        [HttpPut("{name}")]
+        public void Put(string name, [FromBody] string value)
+        {
+
+        }
+
         [HttpDelete("{id}")]
         public void Delete(int id)
+        {
+        }
+
+        [HttpDelete("{name}")]
+        public void Delete(string name)
         {
         }
     }

ed46be6c28a01a2a93e9ac214a9e3256f9bc5191
Camila <camiluc.peli@gmail.com>
2019-07-02 15:57:15 +0200

    Adding JobTitleService as a dependency.

--- JobTitles.Server/Startup.cs
+++ JobTitles.Server/Startup.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using JobTitles.Business.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace JobTitles.Server
 {
@@ -26,6 +20,7 @@ namespace JobTitles.Server
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddScoped<JobTitleService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

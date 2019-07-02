9ad53c0b7fadd5fc662c592ab4fd1387ba364371
Camila <camiluc.peli@gmail.com>
2019-07-02 16:57:20 +0200

    Basic Swagger documentation added.

--- JobTitles.Server/Controllers/JobTitleController.cs
+++ JobTitles.Server/Controllers/JobTitleController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace JobTitles.Server.Controllers
 {
+    [Produces("application/json")]
     [Route("api/[controller]")]
     [ApiController]
     public class JobTitleController : ControllerBase
@@ -17,48 +18,84 @@ namespace JobTitles.Server.Controllers
             _service = service;
         }
 
+        /// <summary>
+        /// Provides a JSON with all the Job Titles.
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public async Task<IEnumerable<JobTitle>> Get()
         {
             return await _service.GetAll();
         }
 
+        /// <summary>
+        /// Provides the the information of a Job Title.
+        /// </summary>
+        /// <param name="id">Id of the wanted Job Title.</param>
+        /// <returns>JSON with the Job Title's data.</returns>
         [HttpGet("{id}")]
         public async Task<JobTitle> Get(int id)
         {
             return await _service.Get(id);
         }
 
+        /// <summary>
+        /// Provides the the information of a Job Title.
+        /// </summary>
+        /// <param name="name">Name of the wanted Job Title.</param>
+        /// <returns>JSON with the Job Title's data.</returns>
         [HttpGet("{name}")]
         public async Task<JobTitle> Get(string name)
         {
             return await _service.Get(name);
         }
 
+        /// <summary>
+        /// Inserts a new Job Title with the given data.
+        /// </summary>
+        /// <param name="jobTitle">JSON with the data to insert.</param>
         [HttpPost]
         public async void Post([FromBody] string jobTitle)
         {
             _service.Insert(jobTitle);
         }
 
+        /// <summary>
+        /// Updates the given Job Title.
+        /// </summary>
+        /// <param name="id">The id of the Job Title to update.</param>
+        /// <param name="value">The new data to update.</param>
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
             _service.Update(id, value);
         }
 
+        /// <summary>
+        /// Updates the given Job Title.
+        /// </summary>
+        /// <param name="name">The name og the Job Title to update.</param>
+        /// <param name="value">The new data to update.</param>
         [HttpPut("{name}")]
         public void Put(string name, [FromBody] string value)
         {
             _service.Update(name, value);
         }
 
+        /// <summary>
+        /// Deletes the given Job Title.
+        /// </summary>
+        /// <param name="id">The id of the Job Title to delete.</param>
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
             _service.Delete(id);
         }
 
+        /// <summary>
+        /// Deletes the given Job Title.
+        /// </summary>
+        /// <param name="name">The name of the Job Title to delete.</param>
         [HttpDelete("{name}")]
         public void Delete(string name)
         {
--- JobTitles.Server/Startup.cs
+++ JobTitles.Server/Startup.cs
@@ -4,6 +4,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace JobTitles.Server
 {
@@ -21,6 +25,29 @@ namespace JobTitles.Server
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddScoped<JobTitleService>();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Job Titles' API",
+                    Description = "This API provides information regarding to Job Titles.",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "Camila Lucía Pérez Liria",
+                        Email = "camiluc.peli@gmail.com",
+                        Url = "https://www.linkedin.com/in/camiluc-peli/"
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +64,17 @@ namespace JobTitles.Server
             }
 
             app.UseHttpsRedirection();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Job Titles' API");
+            });
+
             app.UseMvc();
         }
     }
